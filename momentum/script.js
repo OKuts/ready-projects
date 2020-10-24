5a36e988ef2acd0f331de6c35efd1a8c535f9b8e
OKuts <kuts.ol.vas@gmail.com>
2020-10-24 19:48:12 +0300

    feat: momentum

--- momentum/script.js
+++ momentum/script.js
@@ -2,14 +2,15 @@ const time = document.querySelector('.time'),
   greeting = document.querySelector('.greeting'),
   name = document.querySelector('.name'),
   focus = document.querySelector('.focus'),
-  data = document.querySelector('.day');
+  data = document.querySelector('.day'),
+  town = document.querySelector('.town');
 
 const dayWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const nameMonth = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 const btn = document.querySelector('.btn');
 let countBG = new Date().getHours();
-let tempName, tempFocus;
+let tempName, tempFocus, tempTown;
 
 function showTime() {
   let today = new Date(),
@@ -29,34 +30,31 @@ function addZero(n) {
   return (parseInt(n, 10) < 10 ? '0' : '') + n;
 }
 
-function setBG(hour) {
+function setBG(hour, folder) {
+  hour = localStorage.getItem('listImg').split(',')[hour];
   hour = hour < 10 ? `0${+hour}` : +hour;
-  document.body.style.backgroundImage = `url('./assets/images/${hour}.jpg')`;
+  const img = document.createElement('img');
+  img.src = `./assets/images/${folder}/${hour}.jpg`;
+  img.onload = () => {
+    document.body.style.backgroundImage = `url('${img.src}')`;
+  };
 }
 
-function setBGButton() {
-  // hour = hour < 10 ? `0${+hour}` : +hour;
-  // document.body.style.backgroundImage = `url('./assets/images/${hour}.jpg')`;
+function getFolderImg(hour) {
+  if (hour < 6) return ['night', 'Good Night, '];
+  else if (hour < 12) return ['morning', 'Good Morning, '];
+  else if (hour < 18) return ['day', 'Good Afternoon, '];
+  else return ['evening', 'Good Evening, '];
 }
 
 function setBgGreet(btn) {
   let today = new Date(),
-    hour = today.getHours()
-  if (hour < 6) {
-    greeting.textContent = 'Good Night, ';
-    setBG(hour);
-  } else if (hour < 12) {
-    greeting.textContent = 'Good Morning, ';
-    setBG(hour);
-  } else if (hour < 18) {
-    greeting.textContent = 'Good Afternoon, ';
-    setBG(hour);
-  } else {
-    greeting.textContent = 'Good Evening, ';
-    setBG(hour);
-    document.body.style.color = 'white';
-  }
+    hour = today.getHours();
+  const [folder, greet] = getFolderImg(hour);
+  greeting.textContent = greet;
+  setBG(hour, folder);
 }
+
 
 function getName() {
   if (localStorage.getItem('name') === null) {
@@ -69,10 +67,36 @@ function getName() {
 function setName(e) {
   if (e.type === 'keypress') {
     if (e.which == 13 || e.keyCode == 13) {
-      e.target.innerText ? localStorage.setItem('name', e.target.innerText) : name.textContent = tempName;
-      tempName = '';
+      if (e.target.innerText) {
+        localStorage.setItem('name', e.target.innerText);
+        tempName = e.target.innerText;
+      } else name.textContent = tempName;
       name.blur();
     }
+  } else {
+    name.textContent = tempName;
+  }
+}
+
+function getTown() {
+  if (localStorage.getItem('town') === null) {
+    town.textContent = '[Enter Your Town]';
+  } else {
+    town.textContent = localStorage.getItem('town');
+  }
+}
+
+function setTown(e) {
+  if (e.type === 'keypress') {
+    if (e.which == 13 || e.keyCode == 13) {
+      if (e.target.innerText) {
+        tempTown = e.target.innerText;
+        weather(town.textContent)
+      } else town.textContent = tempTown;
+      town.blur();
+    }
+  } else {
+    town.textContent = tempTown;
   }
 }
 
@@ -87,49 +111,116 @@ function getFocus() {
 function setFocus(e) {
   if (e.type === 'keypress') {
     if (e.which == 13 || e.keyCode == 13) {
-      e.target.innerText ? localStorage.setItem('focus', e.target.innerText) : focus.textContent = tempFocus;
-      tempFocus = '';
+      if (e.target.innerText) {
+        localStorage.setItem('focus', e.target.innerText);
+        tempFocus = e.target.innerText;
+      } else focus.textContent = tempFocus;
       focus.blur();
     }
+  } else {
+    focus.textContent = tempFocus;
   }
 }
 
 function delName() {
   tempName = name.textContent;
   name.textContent = '';
-  name.style.width = '200px';
+  name.style.minWidth = '100px';
 }
 
 function delFocus() {
   tempFocus = focus.textContent;
   focus.textContent = '';
-  focus.style.width = '300px';
+  focus.style.minWidth = '300px';
 }
+
+function delTown() {
+  tempTown = town.textContent;
+  town.textContent = '';
+  town.style.minWidth = '100px';
+}
+// ---------------------------------------------------------------------------------------
 const blockquote = document.querySelector('blockquote');
 const figcaption = document.querySelector('figcaption');
 
 async function getQuote() {
-  // const url = `https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en`;
-  // const res = await fetch(url);
-  // const data = await res.json();
-  // blockquote.textContent = data.quoteText;
-  // figcaption.textContent = data.quoteAuthor;
+  const url = `https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en`;
+  const res = await fetch(url);
+  const data = await res.json();
+  blockquote.textContent = data.quoteText;
+  figcaption.textContent = data.quoteAuthor;
+}
+// ---------------------------------------------------------------------------------------
+function setListImg() {
+  let temp;
+  const listImg = [[], [], [], []];
+  const currentDate = new Date().getDate();
+  if (localStorage.getItem('currentDate') === null || localStorage.getItem('currentDate') != currentDate) {
+    listImg.forEach(el => {
+      let i = 0;
+      while (i < 6) {
+        temp = Math.round(Math.random() * 19 + 1);
+        if (!el.includes(temp)) {
+          el.push(temp);
+          i++;
+        }
+      }
+    })
+    localStorage.setItem('listImg', listImg);
+    localStorage.setItem('currentDate', currentDate);
+  }
 }
 
+function weather(sity) {
+  fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${sity}&appid=b983ca3b97b2387c3d05e06b56c1f579&units=metric`)
+    .then(function (resp) {
+      return resp.json()
+    })
+    .then(function (data) {
+      console.log(data.list[0])
+      let icon = data.list[0].weather[0].icon;
+      document.querySelector('.icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${icon}@2x.png">`;
+      document.querySelector('.temperature').innerHTML = 't ' + Math.round(data.list[0].main.temp) + '&deg;';
+      document.querySelector('.humidity').innerHTML = data.list[0].main.humidity + '%';
+      document.querySelector('.wind1').style.transform = `rotate(${data.list[0].wind.deg + 90}deg)`;
+      document.querySelector('.speed').textContent = `${data.list[0].wind.speed} m/s`;
+      document.querySelector('.block-weather').style.display = 'flex';
+      localStorage.setItem('town', sity);
+
+    })
+    .catch(function () {
+      document.querySelector('.town-error').textContent = "Error Town";
+      document.querySelector('.town').textContent = tempTown;
+      setTimeout(() => {
+        document.querySelector('.town-error').textContent = '';
+        town.textContent = localStorage.getItem('town') || '[Enter Your Town]';
+      }, 2000);
+    })
+}
+
+
+// ---------------------------------------------------------------------------------------
 name.addEventListener('focus', delName);
 name.addEventListener('keypress', setName);
 name.addEventListener('blur', setName);
 focus.addEventListener('focus', delFocus);
 focus.addEventListener('keypress', setFocus);
 focus.addEventListener('blur', setFocus);
+town.addEventListener('focus', delTown);
+town.addEventListener('keypress', setTown);
+town.addEventListener('blur', setTown);
 btn.addEventListener('click', function () {
   countBG === 23 ? countBG = 0 : countBG++;
-  setBG(countBG);
+  setBG(countBG, getFolderImg(countBG)[0]);
 });
 
 showTime();
+setListImg();
 setBgGreet();
 getName();
 getFocus();
+getTown();
+if (town.textContent) weather(town.textContent);
+
 
 document.addEventListener('DOMContentLoaded', getQuote);
